dc9f6235a9880a28762729fcb397c394d361e66e
acigorgutierrezdc <ac.igor.gutierrez.dc@gmail.com>
2021-08-15 15:02:35 -0300

    all code done.
    Need fix vuln.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,46 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlunoComponent } from './aluno/aluno.component';
+import { AlunoService } from './aluno.service';  
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';  
 
+import { HttpClientModule, HttpClient } from '@angular/common/http';  
+import {  
+MatButtonModule, MatMenuModule, MatDatepickerModule,MatNativeDateModule , MatIconModule, 
+MatCardModule, MatSidenavModule,MatFormFieldModule,  
+MatInputModule, MatTooltipModule, MatToolbarModule  
+} from '@angular/material';  
+
+import { MatRadioModule } from '@angular/material/radio';  
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';  
 @NgModule({
   declarations: [
     AppComponent,
     AlunoComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule,  
+    FormsModule,  
+    ReactiveFormsModule,  
+    HttpClientModule,  
+    BrowserAnimationsModule,  
+    MatButtonModule,  
+    MatMenuModule,  
+    MatDatepickerModule,  
+    MatNativeDateModule,  
+    MatIconModule,  
+    MatRadioModule,  
+    MatCardModule,  
+    MatSidenavModule,  
+    MatFormFieldModule,  
+    MatInputModule,  
+    MatTooltipModule,  
+    MatToolbarModule,  
+    AppRoutingModule  
   ],
-  providers: [],
+  providers: [HttpClientModule, AlunoService,MatDatepickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
